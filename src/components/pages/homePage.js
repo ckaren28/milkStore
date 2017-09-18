1729a1bb73f66b2ae3f577d8d5310d1020715cc2
Karen <c.karen28@gmail.com>
2017-09-18 00:49:43 -0400

    initial

---
+++ src/components/pages/homePage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Homepage extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+
+        <h1>hi, homepage content here.</h1>
+
+
+      </div>
+    );
+  }
+}
+
+export default Homepage;
